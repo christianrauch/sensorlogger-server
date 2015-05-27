4e573e62fe1144eb9a3e8d80c779ab8d1016f04a
Christian Rauch <Rauch.Christian@gmx.de>
2015-05-28 00:57:36 +0200

    Timer class for use within periodic threads

---
+++ templog-server/Timer.h
@@ -0,0 +1,40 @@
+#include <sys/timerfd.h>
+#include <unistd.h>
+#include <errno.h>
+
+class Timer {
+    private:
+        int fd;
+
+    public:
+        Timer();
+        void set(const time_t period);
+        unsigned long long wait();
+};
+
+Timer::Timer() {
+    fd = 0;
+}
+
+void Timer::set(const time_t period) {
+    // create file descriptor
+    fd = timerfd_create(CLOCK_MONOTONIC, 0);
+    if(fd<0) {
+        std::cerr<<strerror(errno)<<std::endl;
+    }
+
+    // set timer values
+    const timespec time = {.tv_sec = period, .tv_nsec = 0};
+    const itimerspec timer = {.it_interval = time, .it_value = time};
+
+    // activate timer
+    if(timerfd_settime(fd, 0, &timer, NULL) < 0) {
+        std::cout<<strerror(errno)<<std::endl;
+    }
+}
+
+unsigned long long Timer::wait() {
+    unsigned long long missed;
+    read(fd, &missed, sizeof(missed));
+    return missed;
+}
